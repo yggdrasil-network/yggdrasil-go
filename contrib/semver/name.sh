636655825847d6e35ea795f1954729a8f1036fea
Neil Alexander <neilalexander@users.noreply.github.com>
2018-03-05 20:20:17 +0000

    Use new semver versioning for CircleCI builds

---
+++ contrib/semver/name.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Get the branch name
+BRANCH=$(git name-rev --name-only HEAD 2>/dev/null)
+
+# Check if the branch name check fails
+if [ $? != 0 ]; then
+  printf "yggdrasil"
+  exit 0
+fi
+
+# Check if the branch name is not master
+if [ "$BRANCH" = "master" ]; then
+  printf "yggdrasil"
+  exit 0
+fi
+
+# If it is something other than master, append it
+printf "yggdrasil-%s" "$BRANCH"
