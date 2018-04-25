1be8bbed1c8f1682f0d433c9cf1113ffc486ffb6
Jeff Becker <jeff@i2p.rocks>
2018-04-25 10:09:09 -0400

    more docs

--- src/yggdrasil/config/i2p.go
+++ src/yggdrasil/config/i2p.go
@@ -1,7 +1,8 @@
 package config
 
+// I2PConfig is the configuration structure for i2p related configuration
 type I2PConfig struct {
-	Keyfile string
-	Addr    string
+	Keyfile string // private key file or empty string for ephemeral keys
+	Addr    string // address of i2p api connector
 	Enabled bool
 }
--- src/yggdrasil/config/tor.go
+++ src/yggdrasil/config/tor.go
@@ -1,11 +1,9 @@
 package config
 
-/**
-*tor specific configuration
- */
+// TorConfig is the configuration structure for Tor Proxy related values
 type TorConfig struct {
-	OnionKeyfile string
-	SocksAddr    string
-	UseForAll    bool
-	Enabled      bool
+	OnionKeyfile string // hidden service private key for ADD_ONION (currently unimplemented)
+	SocksAddr    string // tor socks address
+	UseForAll    bool   // use tor proxy for all connections?
+	Enabled      bool   // use tor at all ?
 }
