a7f5c427d467f9f5db984a3eba6c1a0e7ccd26a7
Neil Alexander <neilalexander@users.noreply.github.com>
2018-12-03 17:44:26 +0000

    Tag releases in master using CI (also checks for v0.x.0 instead of v0.x when deciding version numbers)

--- contrib/semver/version.sh
+++ contrib/semver/version.sh
@@ -4,7 +4,7 @@
 DEVELOPBRANCH="yggdrasil-network/develop"
 
 # Get the last tag
-TAG=$(git describe --abbrev=0 --tags --match="v[0-9]*\.[0-9]*" 2>/dev/null)
+TAG=$(git describe --abbrev=0 --tags --match="v[0-9]*\.[0-9]*\.0" 2>/dev/null)
 
 # Get last merge to master
 MERGE=$(git rev-list $TAG..master --grep "from $DEVELOPBRANCH" 2>/dev/null | head -n 1)
@@ -31,8 +31,12 @@ MINOR=$(echo $TAG | cut -c 2- | cut -d "." -f 2)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Output in the desired format
-if [ $PATCH = 0 ]; then
-  printf 'v%d.%d' "$MAJOR" "$MINOR"
+if [ $PATCH == 0 ]; then
+  if [ ! -z $FULL ]; then
+    printf 'v%d.%d.0' "$MAJOR" "$MINOR"
+  else
+    printf 'v%d.%d' "$MAJOR" "$MINOR"
+  fi
 else
   printf 'v%d.%d.%d' "$MAJOR" "$MINOR" "$PATCH"
 fi
@@ -43,4 +47,3 @@ if [ $BRANCH != "master" ]; then
     printf -- "-%04d" "$BUILD"
   fi
 fi
-
