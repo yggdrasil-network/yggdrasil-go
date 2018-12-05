eae8f9a666ce0b8e59deb1b5fa335e892d136df0
Neil Alexander <neilalexander@users.noreply.github.com>
2018-12-05 22:39:04 +0000

    Try to SO_REUSEPORT on UNIX platforms

---
+++ src/yggdrasil/multicast_unix.go
@@ -0,0 +1,22 @@
+// +build linux darwin netbsd freebsd openbsd dragonflybsd
+
+package yggdrasil
+
+import "syscall"
+import "golang.org/x/sys/unix"
+
+func multicastReuse(network string, address string, c syscall.RawConn) error {
+	var control error
+	var reuseport error
+
+	control = c.Control(func(fd uintptr) {
+		reuseport = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+	})
+
+	switch {
+	case reuseport != nil:
+		return reuseport
+	default:
+		return control
+	}
+}
