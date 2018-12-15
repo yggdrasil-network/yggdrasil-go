2c68d41409aebd24f4b3a96ca124d1973a73ba88
Arceliar <Arceliar@users.noreply.github.com>
2018-12-14 18:30:36 -0600

    move files, still need to fix exports and compile errors

--- src/yggdrasil/core.go
+++ src/yggdrasil/core.go
@@ -8,7 +8,9 @@ import (
 	"net"
 	"regexp"
 
+	"github.com/yggdrasil-network/yggdrasil-go/src/address"
 	"github.com/yggdrasil-network/yggdrasil-go/src/config"
+	"github.com/yggdrasil-network/yggdrasil-go/src/crypto"
 	"github.com/yggdrasil-network/yggdrasil-go/src/defaults"
 )
 
