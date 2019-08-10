589ad638eab4a38060f39061c00ad81be7cc2fbe
Slex <slex@criaz.ru>
2019-08-11 00:31:22 +0300

    Implement feature from https://github.com/yggdrasil-network/yggdrasil-go/issues/488

---
+++ src/version/version.go
@@ -0,0 +1,22 @@
+package version
+
+var buildName string
+var buildVersion string
+
+// BuildName gets the current build name. This is usually injected if built
+// from git, or returns "unknown" otherwise.
+func BuildName() string {
+	if buildName == "" {
+		return "yggdrasilctl"
+	}
+	return buildName
+}
+
+// BuildVersion gets the current build version. This is usually injected if
+// built from git, or returns "unknown" otherwise.
+func BuildVersion() string {
+	if buildVersion == "" {
+		return "unknown"
+	}
+	return buildVersion
+}
