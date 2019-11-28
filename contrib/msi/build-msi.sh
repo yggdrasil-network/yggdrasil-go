88653861c7134550144727210c89dc31b55cc613
Neil Alexander <neilalexander@users.noreply.github.com>
2019-11-28 00:10:49 +0000

    AllowDowngrades

--- contrib/msi/build-msi.sh
+++ contrib/msi/build-msi.sh
@@ -99,8 +99,7 @@ cat > wix.xml << EOF
       SummaryCodepage="1252" />
 
     <MajorUpgrade
-      AllowDowngrades="yes"
-      AllowSameVersionUpgrades="yes" />
+      AllowDowngrades="yes" />
 
     <Media
       Id="1"
