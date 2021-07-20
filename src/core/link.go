cacad8220e671ce4701d921227bf1655f2ddc9a5
TomZ <tom@flowee.org>
2021-07-20 22:02:26 +0200

    Make message clearer and downgrade

--- src/core/link.go
+++ src/core/link.go
@@ -169,7 +169,7 @@ func (intf *link) handler() (chan struct{}, error) {
 		return nil, errors.New("failed to decode metadata")
 	}
 	if !meta.check() {
-		intf.links.core.log.Errorf("Failed to connect to node: %s is incompatible version (local %s, remote %s)",
+		intf.links.core.log.Debugf("Rejected incoming connection: %s is incompatible version (local %s, remote %s)",
 			intf.lname,
 			fmt.Sprintf("%d.%d", base.ver, base.minorVer),
 			fmt.Sprintf("%d.%d", meta.ver, meta.minorVer),
