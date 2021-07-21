f8334c06ff290ffdb19563148ab49a9f5c9f7d42
TomZ <tom@flowee.org>
2021-07-21 19:42:35 +0200

    Differentiate between incoming and outgoing conn

--- src/core/link.go
+++ src/core/link.go
@@ -169,7 +169,14 @@ func (intf *link) handler() (chan struct{}, error) {
 		return nil, errors.New("failed to decode metadata")
 	}
 	if !meta.check() {
-		intf.links.core.log.Debugf("Rejected incoming connection: %s is incompatible version (local %s, remote %s)",
+		var connectError string
+		if intf.incoming {
+			connectError = "Rejected incoming connection"
+		} else {
+			connectError = "Failed to connect"
+		}
+		intf.links.core.log.Debugf("%s: %s is incompatible version (local %s, remote %s)",
+			connectError,
 			intf.lname,
 			fmt.Sprintf("%d.%d", base.ver, base.minorVer),
 			fmt.Sprintf("%d.%d", meta.ver, meta.minorVer),
