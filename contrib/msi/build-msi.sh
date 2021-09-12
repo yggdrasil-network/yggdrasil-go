740d11a51ecc953752f7c8addb5d5b22d9fb1bdc
vadym <vadym.vikulin@gmail.com>
2021-09-13 00:18:57 +0300

    reference fix

--- contrib/msi/build-msi.sh
+++ contrib/msi/build-msi.sh
@@ -243,7 +243,6 @@ cat > wix.xml << EOF
 
     <!-- Step 2: Add UI to your installer / Step 4: Trigger the custom action -->
     <UI>
-        <UIRef Id="WixUI" />
         <Publish Dialog="ExitDialog"
             Control="Finish"
             Event="DoAction"
