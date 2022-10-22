c6289577b73de635acb3372fcd57cfa8d8846e7d
Neil Alexander <neilalexander@users.noreply.github.com>
2022-10-22 17:05:33 +0100

    Update `yggdrasilctl`

--- cmd/yggdrasilctl/main.go
+++ cmd/yggdrasilctl/main.go
@@ -174,16 +174,16 @@ func run() int {
 		if err := json.Unmarshal(recv.Response, &resp); err != nil {
 			panic(err)
 		}
-		table.SetHeader([]string{"Port", "Priority", "Public Key", "IP Address", "Uptime", "RX", "TX", "URI"})
+		table.SetHeader([]string{"Port", "Public Key", "IP Address", "Uptime", "RX", "TX", "Pr", "URI"})
 		for _, peer := range resp.Peers {
 			table.Append([]string{
 				fmt.Sprintf("%d", peer.Port),
-				fmt.Sprintf("%d", peer.Priority),
 				peer.PublicKey,
 				peer.IPAddress,
 				(time.Duration(peer.Uptime) * time.Second).String(),
 				peer.RXBytes.String(),
 				peer.TXBytes.String(),
+				fmt.Sprintf("%d", peer.Priority),
 				peer.Remote,
 			})
 		}
