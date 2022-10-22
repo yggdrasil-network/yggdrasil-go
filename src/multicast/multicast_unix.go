0a1a155e66ace9ee30841348a3eb1cdbafca2264
Neil Alexander <neilalexander@users.noreply.github.com>
2022-10-22 14:56:29 +0100

    Use `SO_REUSEADDR` instead of `SO_REUSEPORT` on Linux

--- src/multicast/multicast_unix.go
+++ src/multicast/multicast_unix.go
@@ -15,15 +15,19 @@ func (m *Multicast) _multicastStarted() {
 
 func (m *Multicast) multicastReuse(network string, address string, c syscall.RawConn) error {
 	var control error
-	var reuseport error
+	var reuseaddr error
 
 	control = c.Control(func(fd uintptr) {
-		reuseport = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+		// Previously we used SO_REUSEPORT here, but that meant that machines running
+		// Yggdrasil nodes as different users would inevitably fail with EADDRINUSE.
+		// The behaviour for multicast is similar with both, so we'll use SO_REUSEADDR
+		// instead.
+		reuseaddr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
 	})
 
 	switch {
-	case reuseport != nil:
-		return reuseport
+	case reuseaddr != nil:
+		return reuseaddr
 	default:
 		return control
 	}
