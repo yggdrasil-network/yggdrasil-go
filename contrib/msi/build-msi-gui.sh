03974a3b5433f8cc27ed0c13bba3b60a3bdc4e0f
vadym <vadym.vikulin@gmail.com>
2022-12-03 17:26:45 +0200

    typo fix

--- contrib/msi/build-msi-gui.sh
+++ contrib/msi/build-msi-gui.sh
@@ -157,7 +157,7 @@ cat > wix.xml << EOF
       EmbedCab="yes"
       CompressionLevel="high" />
 
-    <Icon Id="icon.ico" SourceFile="iv.ico"/>
+    <Icon Id="icon.ico" SourceFile="riv.ico"/>
 
     <Property Id="SOURCEDIRECTORY" Value="${PKGUIFOLDER}" />
     <Property Id="ARPPRODUCTICON" Value="icon.ico" />
