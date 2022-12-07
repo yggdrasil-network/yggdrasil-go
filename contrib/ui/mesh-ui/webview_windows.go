881889c65e2223b2b9bab0739d2cae2a9d3167c6
vadym <vadym.vikulin@gmail.com>
2022-12-07 21:14:29 +0200

    return syscall

--- contrib/ui/mesh-ui/webview_windows.go
+++ contrib/ui/mesh-ui/webview_windows.go
@@ -13,7 +13,7 @@ var riv_ctrl_path string
 func run_command(command string) []byte {
 	args := []string{"-json", command}
 	cmd := exec.Command(riv_ctrl_path, args...)
-	//cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		//log.Fatalf("cmd.Run() failed with %s\n", err)
@@ -25,7 +25,7 @@ func run_command(command string) []byte {
 func run_command_with_arg(command string, arg string) []byte {
 	args := []string{"-json", command, arg}
 	cmd := exec.Command(riv_ctrl_path, args...)
-	//cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		//log.Fatalf("command failed: %s\n", riv_ctrl_path+" "+strings.Join(args, " "))
