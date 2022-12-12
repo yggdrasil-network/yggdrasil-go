ec6e722c1cb66eea81326cea91458f39f1087041
Mihail Slobodyanuk <slobodyanukma@gmail.com>
2022-12-12 14:14:11 +0200

    Fix macos run mesh-ui plist RIVM-20

--- contrib/ui/mesh-ui/webview.go
+++ contrib/ui/mesh-ui/webview.go
@@ -113,7 +113,7 @@ func main() {
 			nopanic(errors.New("Index file not found: " + err.Error()))
 		}
 		if stat, err := os.Stat(confui.IndexHtml); err != nil {
-			nopanic(errors.New("Index file not found or permissians denied: " + err.Error()))
+			nopanic(errors.New(fmt.Sprintf("Index file %v not found or permissians denied: %v", confui.IndexHtml, err.Error())))
 		} else if stat.IsDir() {
 			nopanic(errors.New(fmt.Sprintf("Index file %v not found", confui.IndexHtml)))
 		}
