6f8a9da19f33a62dbf7a2e81e8f7f1a46238d405
vadym <vadym.vikulin@gmail.com>
2022-12-12 20:30:33 +0200

    added get_user_home_path nethod for other OS

--- contrib/ui/mesh-ui/webview_other.go
+++ contrib/ui/mesh-ui/webview_other.go
@@ -3,5 +3,16 @@
 
 package main
 
+import "os"
+
+func get_user_home_path() string {
+	path, exists := os.LookupEnv("HOME")
+	if exists {
+		return path
+	} else {
+		return ""
+	}
+}
+
 func Console(show bool) {
 }
