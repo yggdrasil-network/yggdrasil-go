39711d4c87cbc037955a6abefe1abb98939a0f92
mirefly42 <143206683+mirefly42@users.noreply.github.com>
2025-05-02 17:08:02 +0700

    Prevent contrib/docker/entrypoint.sh script from generating a config readable by everyone

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -6,7 +6,7 @@ CONF_DIR="/etc/yggdrasil-network"
 
 if [ ! -f "$CONF_DIR/config.conf" ]; then
   echo "generate $CONF_DIR/config.conf"
-  yggdrasil --genconf > "$CONF_DIR/config.conf"
+  (umask 037 && yggdrasil --genconf > "$CONF_DIR/config.conf")
 fi
 
 yggdrasil --useconf < "$CONF_DIR/config.conf"
